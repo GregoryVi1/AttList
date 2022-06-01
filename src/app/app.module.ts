de39a489e74768b503654160c398b3aade009aa0
GregoryVi1 <Gregoryvieir@gmail.com>
2022-05-31 22:19:08 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TarefaServico } from './servico/tarefa-servico';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, TarefaServico],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/shared/nova-tarefa/nova-tarefa.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import { Tarefa } from 'src/app/modelos/tarefa';
+@Component({
+  selector: 'app-nova-tarefa',
+  templateUrl: './nova-tarefa.component.html',
+  styleUrls: ['./nova-tarefa.component.scss'],
+})
+export class NovaTarefaComponent implements OnInit {
+
+  constructor(public alertController: AlertController,
+    public toastController: ToastController,
+   ) { }
+
+
+  tarefa: Tarefa = new Tarefa();
+
+  listaDeTarefas: Array<Tarefa> = [];
+
+
+  async presentAlert(titulo: string, subtitulo: string, message: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: titulo,
+      subHeader: subtitulo,
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Sua tarefa foi salva.',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  addNovaTarefa(): void {
+    if (this.tarefa.nome == undefined || this.tarefa.nome == '') {
+
+      this.presentAlert('Atenção', '', 'Informe o nome da tarefa');
+      return;
+    }
+
+    this.tarefa.idStatus = 1;
+    this.tarefa.prioridade = this.listaDeTarefas.length;
+    this.tarefa.obterIcone();
+
+    this.listaDeTarefas.push(this.tarefa);
+
+    this.tarefa == null;
+    this.tarefa = new Tarefa();
+    this.presentToast();
+  }
